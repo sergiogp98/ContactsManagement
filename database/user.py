05417bd7655a1d8150b7cf5dc8cc292f6b64b101
Sergio Garcia Prados <garcapradossergi76@gmail.com>
2019-09-30 10:57:40 +0200

    Impresion de usuarios en json

--- database/user.py
+++ database/user.py
@@ -7,15 +7,6 @@ class User(db.Document):
     surname = db.StringField(required=True, max_length=50)
     email = db.StringField(required=True, max_length=50, unique=True)
 
-    def set_user_name(self, new_name):
-        self.name = new_name
-    
-    def set_user_surname(self, new_surname):
-        self.surname = new_surname
-    
-    def set_user_email(self, new_email):
-        self.email = new_email
-
     def print_users(self):
         users = User.objects()
         return users.to_json()
