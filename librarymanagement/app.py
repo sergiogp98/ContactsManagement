de75f71669e1f087acd618a072e6dee0e67a9780
Sergio Garcia Prados <garcapradossergi76@gmail.com>
2019-10-14 00:02:23 +0200

    Borrado proyecto antiguo

--- librarymanagement/app.py
+++
@@ -1,8 +0,0 @@
-from user import User
-import mongoengine as db
-
-db.connect('LibraryManagement', host='127.0.0.1', port=27017)
-
-user = User(name='Ruben', surname='Garcia', email='d@a.com')
-user.save()
-#print(user.print_users())  
