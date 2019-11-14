5ad03354bf3f1c5f6cd6c4e61795044c0ba9ac8b
Sergio Garcia Prados <garcapradossergi76@gmail.com>
2019-11-14 19:45:27 +0100

    Check ci error

--- models/dbHandler.js
+++ models/dbHandler.js
@@ -11,8 +11,9 @@ require('dotenv').config();
         const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@clustersergio-czq9b.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
 
 function createDB(db, schema){
-    console.log('LLEGA');
     schema.set("collection", db);
+    console.log('LLEGA');
+
     Model = mongoose.model(db, schema);
     Model.createCollection();
     return Model;
