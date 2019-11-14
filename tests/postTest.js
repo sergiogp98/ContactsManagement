453979241ac3a463873c116f0913d289015d8318
Sergio Garcia Prados <garcapradossergi76@gmail.com>
2019-11-14 20:25:25 +0100

    Check ci error

--- tests/postTest.js
+++ tests/postTest.js
@@ -1,6 +1,7 @@
 const modules = require('../modules');
 const supertest = require('supertest');
 const app = require('../controllers/routes');
+const moongose = require('mongoose');
 require('dotenv').config();
 
 const film = {
@@ -11,11 +12,12 @@ const film = {
     summary: 'In Gotham City, mentally-troubled comedian Arthur Fleck embarks on a downward-spiral of social revolution and bloody crime. This path brings him face-to-face with his infamous alter-ego: "The Joker"',
     director: 'Todd Phillips'
 }; 
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@clustersergio-czq9b.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
 
 describe('POST /film', function(){
     before(async () => {
         try{
-            await modules.connectDB();
+            await moongose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
             process.env.USE_TEST_DB = true;
         } catch(err) {
             console.log(err);
@@ -24,8 +26,8 @@ describe('POST /film', function(){
     });
 
     after(async () => {
-        await modules.deleteFilmTitle(film.title);
-        await modules.disconnectDB();
+        //await modules.deleteFilmTitle(film.title);
+        await moongose.disconnect();
         process.env.USE_TEST_DB = false;
     });
 
