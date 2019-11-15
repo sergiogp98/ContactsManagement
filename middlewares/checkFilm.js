1e02efef62b87228392d93833180806bf2c78c66
Sergio Garcia Prados <garcapradossergi76@gmail.com>
2019-11-15 03:07:35 +0100

    Check ci error

--- middlewares/checkFilm.js
+++ middlewares/checkFilm.js
@@ -2,7 +2,7 @@ const modules = require('../modules');
 require('dotenv').config();
 
 async function checkExistsFilm(film){
-    console.log(process.env.FILM_COLLECTION_NAME)
+    console.log('Film')
     const model = await modules.getDB(process.env.FILM_COLLECTION_NAME);
     console.log('LLEGA DESPUES')
     const exists = await model.exists({
