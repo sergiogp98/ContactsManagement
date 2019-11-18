3d237020e921940825c23f6be429abed9d016f3c
Sergio Garcia Prados <garcapradossergi76@gmail.com>
2019-11-18 12:53:16 +0100

    Añadido integración de pm2 en heroku

---
+++ ecosystem.cofig.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [{
+        name: "app",
+        script: "./app.js",
+        instances: "max",
+        exec_mode: "cluster",
+        env: {
+            NODE_ENV: "development"
+        },
+        env_production: {
+            NODE_ENV: "production"
+        }
+    }]
+}
